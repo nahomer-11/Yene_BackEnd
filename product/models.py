f846b03792043da644e5dbd6a642f5dc9d2a8623
NahoMer <nahomer.tech@gmail.com>
2025-06-15 15:57:41 -0300

    Update models.py

--- product/models.py
+++ product/models.py
@@ -21,6 +21,11 @@ class ProductVariant(models.Model):
     extra_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['product']),
+        ]
     
     def __str__(self):
         return f'{self.product.name} - {self.color} - {self.size}'
