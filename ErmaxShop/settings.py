1b046caa602b6b5419fefdb3227fb2ee00f90246
NahoMer <nahomer.tech@gmail.com>
2025-06-15 15:58:32 -0300

    Update settings.py

--- ErmaxShop/settings.py
+++ ErmaxShop/settings.py
@@ -134,7 +134,12 @@ DATABASES = {
     }
 }
 
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'order-variants',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
