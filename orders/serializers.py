a688476872b0e49d6537846b15392c2143bdd928
NahoMer <nahomer.tech@gmail.com>
2025-06-15 17:14:51 -0300

    Update serializers.py

--- orders/serializers.py
+++ orders/serializers.py
@@ -22,10 +22,6 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = ['product_variant', 'product', 'quantity']
 
-    def validate(self, data):
-        if not data.get('product_variant') and not data.get('product'):
-            raise serializers.ValidationError("Either 'product_variant' or 'product' must be provided.")
-        return data
 
 
 # ✅ Used for create/update
@@ -34,39 +30,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = '__all__'
-
-    def create(self, validated_data):
-        items_data = validated_data.pop("items", [])
-        order = Order.objects.create(**validated_data)
-
-        for item_data in items_data:
-            quantity = item_data.get("quantity", 1)
-            product_variant = item_data.get("product_variant")
-            product = item_data.get("product")
-
-            if not product_variant:
-                if not product:
-                    raise serializers.ValidationError("Product ID is required if product_variant is not provided.")
-                product_variant, _ = ProductVariant.objects.get_or_create(
-                    product=product,
-                    color="Default",
-                    size="One Size",
-                    defaults={"extra_price": Decimal('0.00')}
-                )
-
-            price = product_variant.product.base_price + (product_variant.extra_price or Decimal('0.00'))
-            total_price = price * quantity
-
-            OrderItem.objects.create(
-                order=order,
-                product_variant=product_variant,
-                quantity=quantity,
-                price_per_unit=price,
-                total_price=total_price
-            )
-
-        return order
+        read_only_fields = ['order_code', 'status', 'total_price', 'paid_amount', 'payment_method', 'user']
 
 
 # ✅ Used for GET (read-only): includes full nested product_variant
